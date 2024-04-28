dccd6d2e9acdb40271703ec0e79aa934276d7fab
Petr Fifka <petrfifk@gmail.com>
2024-04-28 22:50:40 +0200

    add AI implementation v0.0.1

--- src/ai/generateCypressCode.js
+++ src/ai/generateCypressCode.js
@@ -1,7 +1,6 @@
-const generateCypressCode = async (description, identifier, steps) => {
-  // TODO: implementovat AI
-  // ? Kroky:
-  /*
+// TODO: implementovat AI
+// ? Kroky:
+/*
     1. Příprava promptu pro OpenAI
         a. Připravit vzor promptu
         b. Zpracovat parametry: description, identifier, steps
@@ -18,9 +17,50 @@ const generateCypressCode = async (description, identifier, steps) => {
         b. Získat debugResponse
     5. Vrátit výsledek
   */
+import OpenAI from "openai";
+import dotenv from "dotenv";
+
+const generateCypressCode = async (description, identifier, steps) => {
+  dotenv.config();
+  const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
+
+  const openai = new OpenAI({
+    apiKey: apiKey,
+  });
+
+  // Příprava promptu
+  const promptInit =
+    "Generate Cypress test code with the following instrocutions. Return only the code in the response using Javascript language.";
+  const promptDescription = `Description: ${description}`;
+  const promptIdentifier = `Identifier: ${identifier}`;
+  const promptSteps = `Steps: ${steps}`;
+  let prompt;
+
+  if (steps) {
+    prompt = `${promptInit}\n${promptDescription}\n${promptIdentifier}\n${promptSteps}`;
+  } else {
+    prompt = `${promptInit}\n${promptDescription}\n${promptIdentifier}`;
+  }
+
+  // Příprava requestu
+  const gptResponse = await openai.complete({
+    engine: "gpt-3.5-turbo-0125",
+    prompt: prompt,
+    maxTokens: 500,
+    messages: [
+      {
+        role: "system",
+        content: prompt,
+      },
+    ],
+  });
+
+  // Debug do konzole
+  console.log(JSON.stringify(gptResponse, null, 2));
+
   return {
-    testCode: `tady bude vygenerovaný kód pro test s popisem: ${description}, identifikátorem: ${identifier} \na kroky: ${steps}`,
-    debugResponse: "Tady bude celá odpověď z API...",
+    testCode: gptResponse.choices[0].message,
+    debugResponse: JSON.stringify(gptResponse, null, 2),
   };
 };
 
